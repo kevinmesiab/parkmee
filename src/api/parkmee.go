673da73f35b965b0b8c6853ca617083277d6f99f
Kevin Mesiab <kmesiab@gmail.com>
2020-12-06 23:16:28 -0800

    pkm-1 Create webserver Stub a conceptual driveway object.

--- src/api/parkmee.go
+++ src/api/parkmee.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world");
+	handleRequests()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page apparently.")
+	fmt.Fprintf(w, "Endpoint: HOME")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
---
+++ src/api/types/plots/driveway.go
@@ -0,0 +1,12 @@
+package plots
+
+import (
+	s2 "github.com/golang/geo/s2"
+)
+//
+// Driveway represents a single
+/// rentable location
+//
+type Driveway struct {
+	Location s2.Shape
+}
